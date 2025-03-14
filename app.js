4f5f69dd5de6edc8c793bb715cca88c1b36d6832
Giovanne Silveira <123279931+GiovanneGSH@users.noreply.github.com>
2025-03-14 10:55:25 -0300

    ajustando funções

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
+
+
 let amigos = [];
 
+let listaAmigos = document.getElementById('listaAmigos').innerHTML = 'TESTE';
 
-let listaAmigos = document.getElementById('listaAmigos').innerHTML = '';
+let resultado = document.getElementById('resultado').innerHTML = '1234';
 
-let resultado = document.getElementById('resultado').innerHTML = '';
+
 
 function adicionarAmigo(event){
+
     let nomeDigitado = document.getElementById('amigo').value;
 
-        if (nomeDigitado == ''){
-           alert('Por favor, insira um nome.');
-          
+    if (nomeDigitado !== '') {
+
+        amigos.push(nomeDigitado);
+
+        console.log(amigos);
+    }
+
+    for (let i = 0; i < amigos.length; i++) {
+
+        let texto = amigos.toString();
+
+        listaAmigos.value = texto;
+
+
+        // console.log(amigos[i]);
+
+        // const element = array[index];
+        
+    }
+
+        if(nomeDigitado == ''){
+            alert('Por favor, insira um nome.');
         }
 
-        else{
-            amigos.push(nomeDigitado);
-            
-        }
-        
 };
 
-amigos.forEach(amigo => {
-
-    console.log(amigo)
-    
-}); 
+ 
