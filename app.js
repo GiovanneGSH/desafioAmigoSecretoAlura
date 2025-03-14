ddaa1e6a01973f6dbb1edc6e384af348efb55aac
Giovanne Silveira <123279931+GiovanneGSH@users.noreply.github.com>
2025-03-13 23:14:05 -0300

    criando funções

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+
+let listaAmigos = document.getElementById('listaAmigos').innerHTML = '';
+
+let resultado = document.getElementById('resultado').innerHTML = '';
+
+function adicionarAmigo(event){
+    let nomeDigitado = document.getElementById('amigo').value;
+
+        if (nomeDigitado == ''){
+           alert('Por favor, insira um nome.');
+          
+        }
+
+        else{
+            amigos.push(nomeDigitado);
+            
+        }
+        
+};
+
+amigos.forEach(amigo => {
+
+    console.log(amigo)
+    
+}); 
