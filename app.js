73e94caf816e1f1053774246279c7dea37128eba
Giovanne Silveira <123279931+GiovanneGSH@users.noreply.github.com>
2025-03-15 17:33:32 -0300

    Ajuste JavaScript completo

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-
-
 let amigos = [];
 
-let listaAmigos = document.getElementById('listaAmigos').innerHTML = 'TESTE';
+let listaAmigos = document.getElementById('listaAmigos');
 
-let resultado = document.getElementById('resultado').innerHTML = '1234';
+let resultado = document.getElementById('resultado');
 
-
-
-function adicionarAmigo(event){
+function adicionarAmigo() {
 
     let nomeDigitado = document.getElementById('amigo').value;
 
@@ -18,26 +14,41 @@ function adicionarAmigo(event){
 
         amigos.push(nomeDigitado);
 
-        console.log(amigos);
-    }
+        document.getElementById('amigo').value = '';
 
-    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos.innerHTML = '';
 
-        let texto = amigos.toString();
-
-        listaAmigos.value = texto;
-
-
-        // console.log(amigos[i]);
-
-        // const element = array[index];
-        
-    }
-
-        if(nomeDigitado == ''){
-            alert('Por favor, insira um nome.');
+        for (amigo of amigos) {
+            let itemLista = document.createElement('li');
+            itemLista.textContent = amigo;
+            listaAmigos.appendChild(itemLista);
         }
+
+    }
+    else
+        alert('Por favor, insira um nome.');
 
 };
 
- 
+function sortearAmigo() {
+
+    if (amigos == '') {
+
+        alert('Por favor, insira um nome.');
+
+    }
+    else {
+
+        let indiceArray = Math.floor(Math.random() * amigos.length);
+
+        let amigoSorteado = amigos[indiceArray];
+
+        resultado.innerHTML = `O amigo secreto sorteado é: ${amigoSorteado}`;
+
+        listaAmigos.innerHTML = '';
+
+        amigos = [];
+    }
+
+};
+
